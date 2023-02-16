47ea9f801fb8776735a17a2d92c34badba9354cd
Cesar Ortiz <ceortiz@bancolombia.com.co>
2023-02-16 17:21:07 -0500

    Bonus ninja added

--- src/main/java/org/example/Museo.java
+++ src/main/java/org/example/Museo.java
@@ -1,18 +1,30 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class Museo {
     public static void main(String[] args) {
+        ArrayList<Arte> Museo = new ArrayList<Arte>();
+
         Pintura oleo = new Pintura("Oleo");
-        Pintura acuarela = new Pintura("acuarela");
-        Pintura acrilico = new Pintura("acrilico");
+        Museo.add(oleo);
+        Pintura acuarela = new Pintura("Acuarela");
+        Museo.add(acuarela);
+        Pintura acrilico = new Pintura("Acrilico");
+        Museo.add(acrilico);
 
         Escultura marmol = new Escultura("Marmol");
+        Museo.add(marmol);
         Escultura bronce = new Escultura("Bronce");
+        Museo.add(bronce);
 
-        oleo.verArte();
-        acuarela.verArte();
-        acrilico.verArte();
-        marmol.verArte();
-        bronce.verArte();
+        Random random = new Random();
+        Collections.shuffle(Museo, random);
+
+        for (Arte i: Museo) {
+            i.verArte();
+        }
     }
 }
